23f0f216dfdc3049a1cf945e36e4bb297d344bf0
ThanawatTh <tthamniyay@gmail.com>
2019-05-29 13:34:10 +0700

    Init project without Environment

---
+++ Assets/Script/E1ball.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E1ball : MonoBehaviour
+{
+
+    [Header("Unity Setup")]
+    public ParticleSystem deathEffect;
+
+    public int currentHealth = 1;
+    Rigidbody rb;
+    
+
+    public float cudeSize = 0.2f;
+    public int cubeInrow = 5;
+
+    private Animator anim;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        
+
+    }
+
+
+    public void Damage(int damageAmount)
+    {
+
+        currentHealth -= damageAmount;
+        anim.Play("Hit" ,0 ,0);
+        
+        
+
+
+
+        if (currentHealth <= 0)
+        {
+            //Instantiate(deathEffect, transform.position, Quaternion.identity);
+
+            gameObject.SetActive(false);
+
+        }
+
+
+
+    }
+}
