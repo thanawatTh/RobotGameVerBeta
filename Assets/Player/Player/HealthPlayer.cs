42a84c472eb60a0332e0dc9e955f874ba8924621
Maisang <sarusasiri@gmail.com>
2019-05-29 16:26:12 +0700

    Add code hpbar

---
+++ Assets/Player/Player/HealthPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPlayer : MonoBehaviour
+{
+    public HealthContorller healthBar;
+    // Start is called before the first frame update
+
+    /*void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enamy")
+        {
+            if (healthBar)
+            {
+                healthBar.TakeDamge(10);
+                Debug.Log("damage");
+            }
+        }
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enamy") 
+        {
+                     
+                healthBar.TakeDamge(10);
+                Debug.Log("damage");
+            
+        }
+    }
+}
