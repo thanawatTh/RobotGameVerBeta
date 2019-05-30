f7c3f9cd0d71641122601f2e93379a77c31148d6
Maisang <sarusasiri@gmail.com>
2019-05-30 11:08:27 +0700

    Add animation door Edit Floder

---
+++ Assets/Script/Player/PlayerControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    private Rigidbody rigidBody;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    private float timeMove;
+
+    public float forcePower = 100f;
+    public Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        timeMove += Time.deltaTime;
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlan = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if(groundPlan.Raycast(cameraRay,out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.black);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+    }
+
+     void FixedUpdate()
+    {
+        rigidBody.velocity = moveVelocity;
+    }
+}
