6d034f7fda7c9658c084b8c35bb7ce5782c2ad97
Maisang <sarusasiri@gmail.com>
2019-05-31 18:01:38 +0700

    Edit MissileFollowTarget

--- Assets/Script/MissileFollowTarget.cs
+++ Assets/Script/MissileFollowTarget.cs
@@ -6,31 +6,55 @@ public class MissileFollowTarget : MonoBehaviour
 {
     public Transform target;
     public Rigidbody missleRigibody;
-    
+
 
     public float turn;
     public float missleVelocity;
+    
 
     // Start is called before the first frame update
 
     void Start()
     {
+        
         target = GameObject.FindGameObjectWithTag("TestEnamy").transform;
     }
     private void FixedUpdate()
     {
-        missleRigibody.velocity = transform.forward * missleVelocity;
-        var missleTargetRotation = Quaternion.LookRotation(target.position - transform.position);
-        missleRigibody.MoveRotation(Quaternion.RotateTowards(transform.rotation, missleTargetRotation, turn));
         
+      // missleRigibody.velocity = transform.forward * missleVelocity;
+
+       var missleTargetRotation = Quaternion.LookRotation(target.position - transform.position);
+
+        if (missleTargetRotation != null && target != null) 
+        {
+            missleRigibody.velocity = transform.forward * missleVelocity;
+            missleRigibody.MoveRotation(Quaternion.RotateTowards(transform.rotation, missleTargetRotation, turn));
+
+        }
+       
+
+        
+       
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "TestEnamy")
+            {
+
+                Destroy(gameObject);
+                Debug.Log("i found target");
+            }
+
+        else
         {
-            Destroy(gameObject);
-            Debug.Log("i find target");
+
         }
+
+        
+           
     }
+
 }
--- Assets/Script/Missiles.cs
+++ Assets/Script/Missiles.cs
@@ -1,27 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Missiles : MonoBehaviour
 {
     public Rigidbody missilPrefab;
     public Transform barrelEnd;
+    public int countMissiles = 5;
+    public Text countM;
+    public bool isMissleGo;
+    public SkillCoolDown useScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isMissleGo = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Rigidbody missleInstance;
-            missleInstance = Instantiate(missilPrefab, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
-            missleInstance.AddForce(barrelEnd.forward * 5000);
-            Debug.Log("missle");
-        }
 
+       if (countMissiles > 0)
+       {
+            if (Input.GetKeyDown(KeyCode.Alpha1)) 
+            {
+                    MissleMove();
+                    countMissiles--;
+
+                    isMissleGo = true;
+
+            }
+            else
+            {
+                isMissleGo = false;
+            }
+           
+           // countM.text = countMissiles.ToString();
+       }
     }
+      
+    
+
+    void MissleMove()
+    {
+        Rigidbody missleInstance;
+        missleInstance = Instantiate(missilPrefab, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
+        missleInstance.AddForce(barrelEnd.forward * 5000);
+    }
+
+   
 }
