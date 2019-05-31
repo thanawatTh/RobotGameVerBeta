1c1a87ed3150e23ef6054cb2c3e5735f5c19679f
Maisang <sarusasiri@gmail.com>
2019-05-31 18:00:21 +0700

    Add Cooldown

---
+++ Assets/Script/SkillCoolDown.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillCoolDown : MonoBehaviour
+{
+    public Image imageCooldown;
+    public float cooldown = 5;
+    public bool isCooldown;
+    public Missiles useScript;
+
+     void Start()
+    {
+        //useScript = GetComponent<Missiles>();
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (useScript.isMissleGo == true)     
+        {
+            isCooldown = true;
+        }
+
+        if (isCooldown == true )  
+        {
+            imageCooldown.fillAmount += 1 / cooldown * Time.deltaTime;
+
+            if (imageCooldown.fillAmount >= 1)
+            {
+                imageCooldown.fillAmount = 0;
+                isCooldown = false;
+            }
+        }
+
+      
+
+
+    }
+}
