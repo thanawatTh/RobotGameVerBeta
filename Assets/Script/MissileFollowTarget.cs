8ca18e86b2fe7440e8c627a5b2ab0c6b796d0293
Maisang <sarusasiri@gmail.com>
2019-05-31 12:10:05 +0700

    Add Follow target

--- Assets/Script/MissileFollowTarget.cs
+++ Assets/Script/MissileFollowTarget.cs
@@ -5,16 +5,32 @@ using UnityEngine;
 public class MissileFollowTarget : MonoBehaviour
 {
     public Transform target;
+    public Rigidbody missleRigibody;
+    
+
+    public float turn;
+    public float missleVelocity;
 
     // Start is called before the first frame update
+
     void Start()
     {
+        target = GameObject.FindGameObjectWithTag("TestEnamy").transform;
+    }
+    private void FixedUpdate()
+    {
+        missleRigibody.velocity = transform.forward * missleVelocity;
+        var missleTargetRotation = Quaternion.LookRotation(target.position - transform.position);
+        missleRigibody.MoveRotation(Quaternion.RotateTowards(transform.rotation, missleTargetRotation, turn));
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "TestEnamy")
+        {
+            Destroy(gameObject);
+            Debug.Log("i find target");
+        }
     }
 }
