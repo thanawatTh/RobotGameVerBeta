bc356586038993dd952ef6f16e7692c9f422ed5f
Maisang <sarusasiri@gmail.com>
2019-06-10 11:40:46 +0700

    Create TouchControl,Rotation

---
+++ Assets/Script/TouchControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchControl : MonoBehaviour,IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    private RectTransform parent;
+    private float radius;
+    public Vector2 joyInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = this.transform.parent.GetComponent<RectTransform>();
+        radius = parent.sizeDelta.x / 2;
+        joyInput = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+
+        if (this.transform.localPosition.magnitude > radius)
+        {
+            this.transform.localPosition = this.transform.localPosition.normalized * radius;
+
+        }
+
+        joyInput = this.transform.localPosition / radius;
+
+        Debug.Log(joyInput.magnitude);
+    }
+
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        this.transform.localPosition = Vector3.zero;
+        joyInput = Vector2.zero;
+    }
+}
---
+++ Assets/Script/TouchRotControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchRotControl : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    private RectTransform parent;
+    private float radius;
+
+    public Vector2 rotInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = this.transform.parent.GetComponent<RectTransform>();
+        radius = parent.sizeDelta.x / 2;
+        rotInput = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+
+        if (this.transform.localPosition.magnitude > radius)
+        {
+            this.transform.localPosition = this.transform.localPosition.normalized * radius;
+
+        }
+
+
+        rotInput = this.transform.localPosition / radius;
+        Debug.Log(rotInput.magnitude);
+    }
+
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        this.transform.localPosition = Vector3.zero;
+        //rotInput = Vector2.zero;
+    }
+}
