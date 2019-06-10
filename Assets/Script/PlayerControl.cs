7c2b780ee974d2416a49f174a2360d2ab87aedae
Maisang <sarusasiri@gmail.com>
2019-06-10 11:38:40 +0700

    Edit Player

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -13,33 +13,59 @@ public class PlayerControl : MonoBehaviour
     private Camera mainCamera;
 
     public Ray cameraRay;
+
+    TouchControl joyInputMove;
+    TouchRotControl joyInputRot;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         mainCamera = Camera.main;
+
+        joyInputMove = GameObject.Find("JoyMove").GetComponent<TouchControl>();
+        joyInputRot = GameObject.Find("JoyRot").GetComponent<TouchRotControl>();
     }
 
     
     void Update()
     {
-        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        //Touch
+        /////////////////////////////////////////////////////////////////////////////
+        moveInput = new Vector3(joyInputMove.joyInput.x, 0, joyInputMove.joyInput.y);
         moveVelocity = moveInput * moveSpeed;
 
-        cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-        Plane groundPlan = new Plane(Vector3.up, Vector3.zero);
-        float rayLength;
+        Vector2 from = new Vector2(0, 1);
+        Vector2 to = joyInputRot.rotInput;
 
-        if(groundPlan.Raycast(cameraRay,out rayLength))
-        {
-            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
-            Debug.DrawLine(cameraRay.origin, pointToLook, Color.black);
+        transform.localEulerAngles = new Vector3(0f, -30 - Vector2.SignedAngle(from, to), 0f);//หามุมของแกนy
 
-            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
-        }
+
+
+
+        /* KeyBroad
+         /////////////////////////////////////////////////////////////////////////////////////////////
+           moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+           moveVelocity = moveInput * moveSpeed;
+
+           cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+           Plane groundPlan = new Plane(Vector3.up, Vector3.zero);
+           float rayLength;
+
+           if(groundPlan.Raycast(cameraRay,out rayLength))
+           {
+               Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+               Debug.DrawLine(cameraRay.origin, pointToLook, Color.black);
+
+               transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+           }*/
+        /////////////////////////////////////////////////////////////////////////////////////////////
+
     }
 
-     void FixedUpdate()
+    void FixedUpdate()
     {
         rigidBody.velocity = moveVelocity;
     }
