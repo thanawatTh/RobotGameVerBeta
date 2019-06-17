e21631f87a25fe97f725b9ad50426ecb92f921d4
Maisang <sarusasiri@gmail.com>
2019-06-17 18:12:39 +0700

    edit scenes1

---
+++ Assets/Script/scence.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scence : MonoBehaviour
+{
+
+    public GameObject wall;
+    public int enamy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wall.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        SceneManager.LoadScene("LVL02");
+    }
+}
