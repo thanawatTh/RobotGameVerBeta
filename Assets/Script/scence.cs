77dca5a437dae8c361b638a10f5b67e07a8765e8
Maisang <sarusasiri@gmail.com>
2019-06-18 13:43:28 +0700

    no message

--- Assets/Script/scence.cs
+++ Assets/Script/scence.cs
@@ -9,19 +9,19 @@ public class scence : MonoBehaviour
     public GameObject wall;
    
 
-    public GameObject[] danger;
+     
 
     // Start is called before the first frame update
     void Start()
     {
         wall.SetActive(false);
-        danger = GameObject.FindGameObjectsWithTag("TestEnamy");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        CheackDanger();
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -34,21 +34,7 @@ public class scence : MonoBehaviour
     }
 
 
-    public void CheackDanger()
-    {
-        //GameObject enmay = GameObject.FindWithTag("TestEnamy");
-
-        //if (enmay != null && danger.Contains(enmay))
-        //{
-        //    Debug.Log("DangerAll");
-        //}
-
-
-        if (danger != null) 
-        {
-            Debug.Log("dddddddddddddddddddddddddddddddddd");
-        }
-    }
+   
 
   
 }
