f9c8e46d1c5d68adbb01590b0409ae7c04ed0456
Maisang <sarusasiri@gmail.com>
2019-06-19 18:16:44 +0700

    edit canvas

--- Assets/Script/HealthUI.cs
+++ Assets/Script/HealthUI.cs
@@ -5,7 +5,15 @@ using UnityEngine.UI;
 
 public class HealthUI : MonoBehaviour
 {
+    [HideInInspector]
     public Image healthUi;
+    public Text test1;
+    public Text test2;
+    public Text test3;
+    public Text test4;
+    public Image test5;
+    public Camera testCamera;
+    
 
 
     private void Start()
@@ -15,7 +23,14 @@ public class HealthUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 image = Camera.main.WorldToScreenPoint(this.transform.position);
-        healthUi.transform.position = image;
+        Vector3 image = Camera.main.WorldToScreenPoint(this.transform.parent.transform.position);
+        Vector3 image2 = testCamera.WorldToScreenPoint(this.transform.parent.transform.position);
+        healthUi.rectTransform.position = image;
+        test1.rectTransform.position = image;
+        test2.rectTransform.anchoredPosition = image;
+        test3.rectTransform.anchoredPosition = image2;
+        test4.rectTransform.position = image2;
+        test5.rectTransform.position = image2;
+
     }
 }
