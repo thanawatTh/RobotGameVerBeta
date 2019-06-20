5e5e721fca0d4391e828cedb2671dd1c7bc595d0
Maisang <sarusasiri@gmail.com>
2019-06-20 15:34:43 +0700

    edit additive scene

---
+++ Assets/ScenceManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenceManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+        //SceneManager.LoadScene("BaseScenes", LoadSceneMode.Additive);
+        //SceneManager.SetActiveScene(SceneManager.GetSceneByName("BaseScenes"));
+       
+    }
+     void Awake()
+    {
+        SceneManager.LoadScene("BaseScenes", LoadSceneMode.Additive);
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
